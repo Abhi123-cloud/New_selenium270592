781caa3676a5b00821d9e3986239597086848fa7
Abhilash <you@example.com>
2025-03-02 21:31:08 +0530

    added code  fr config fle

--- src/test/java/testCases/BasaClass.java
+++ src/test/java/testCases/BasaClass.java
@@ -1,6 +1,7 @@
 package testCases;
 
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,21 +11,26 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
+
+import io.restassured.internal.support.FileReader;
+
 import org.apache.logging.log4j.LogManager;//log4j
 import org.apache.logging.log4j.Logger;//log4j
 
-public class BasaClass
+public class BasaClass 
 {
 public WebDriver driver;
 public Logger logger;//Create log varibale and import logger 
-	
+	public Properties p;
 	
 //Base class in parent class of all test case
 @Parameters({"browser"})
 @BeforeClass
 void Setup(@Optional("chrome") String br )
 
-{
+{//Loading config.prperties file
+	 
+	
 	//Adding log 4j code s
 	logger=LogManager.getLogger(this.getClass());
 	switch(br.toLowerCase())
